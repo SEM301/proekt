7c1b434baa12dd3b6a6789b510175b828185ebf4
SEM301 <72428570+SEM301@users.noreply.github.com>
2020-10-10 12:36:27 +0900

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp2
+{ class user
+    {   
+        public string name = Console.ReadLine();
+        private int age =Convert.ToInt32(Console.ReadLine());
+        protected string status = "active";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите своё имя и возраст");
+            user one =new user();
+            Console.WriteLine(one.name);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ методы/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Kvadrat();
+            Koren();
+
+
+        }
+        public static void Kvadrat()
+        {
+            Console.WriteLine("Введите число");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = a * a;
+            Console.WriteLine($"Квадрат числа {a}={b}");
+            Console.ReadLine();
+        }
+        private static void Koren()
+        {
+            Console.WriteLine("Введите число");
+            int c = Convert.ToInt32(Console.ReadLine());
+            double d = Math.Sqrt(c);
+            Console.WriteLine($"Квадрат числа {c}={d}");
+            Console.ReadLine();
+        }
+    }
+}
